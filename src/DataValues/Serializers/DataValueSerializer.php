caa34fd4d44a4992af2646ef8b250bd15a24976c
jeroendedauw <jeroendedauw@gmail.com>
2013-11-18 23:45:20 +0100

    Add first test

---
+++ src/DataValues/Serializers/DataValueSerializer.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace DataValues\Serializers;
+
+/**
+ * @since 0.1
+ *
+ * @licence GNU GPL v2+
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ */
+class DataValueSerializer {
+
+
+
+}
---
+++ tests/Serializers/DataValueSerializerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\DataValues\Serializers;
+
+/**
+ * @covers DataValues\Serializers\DataValueSerializer
+ *
+ * @licence GNU GPL v2+
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ */
+class DeserializerFactoryTest extends \PHPUnit_Framework_TestCase {
+
+	public function testTestRuns() {
+		$this->assertTrue(true);
+	}
+
+}
