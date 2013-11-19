5a689a1a5bde842668363dc7eaba2a2982381532
jeroendedauw <jeroendedauw@gmail.com>
2013-11-19 16:29:26 +0100

    Update README and entry point

--- Serialization.php
+++ Serialization.php
@@ -10,22 +10,19 @@
  * @author Jeroen De Dauw < jeroendedauw@gmail.com >
  */
 
-if ( defined( 'DATAVALUES_SERIALIZATION_VERSION' ) ) {
-	// Do not initialize more then once.
-	return 1;
-}
+if ( !defined( 'DATAVALUES_SERIALIZATION_VERSION' ) ) {
+	define( 'DATAVALUES_SERIALIZATION_VERSION', '0.1 alpha' );
 
-define( 'DATAVALUES_SERIALIZATION_VERSION', '0.1 alpha' );
-
-if ( defined( 'MEDIAWIKI' ) ) {
-	$GLOBALS['wgExtensionCredits']['datavalues'][] = array(
-		'path' => __DIR__,
-		'name' => 'DataValues Serialization',
-		'version' => DATAVALUES_SERIALIZATION_VERSION,
-		'author' => array(
-			'[https://www.mediawiki.org/wiki/User:Jeroen_De_Dauw Jeroen De Dauw]'
-		),
-		'url' => 'https://github.com/DataValues/Serialization',
-		'description' => 'Serializers and deserializers for DataValue implementations',
-	);
+	if ( defined( 'MEDIAWIKI' ) ) {
+		$GLOBALS['wgExtensionCredits']['datavalues'][] = array(
+			'path' => __DIR__,
+			'name' => 'DataValues Serialization',
+			'version' => DATAVALUES_SERIALIZATION_VERSION,
+			'author' => array(
+				'[https://www.mediawiki.org/wiki/User:Jeroen_De_Dauw Jeroen De Dauw]'
+			),
+			'url' => 'https://github.com/DataValues/Serialization',
+			'description' => 'Serializers and deserializers for DataValue implementations',
+		);
+	}
 }
